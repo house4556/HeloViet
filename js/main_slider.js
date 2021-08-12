9575a89050f8d57580ef622320956f944f5f7cfb
kang <house4556@naver.com>
2021-08-12 15:26:48 +0900

    first Commit

---
+++ js/main_slider.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $('#aside__shop').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: false,
+        autoplay : true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        navText: [
+            "<i class='bx bx-first-page'></i>",
+            "<i class='bx bx-last-page' ></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            768: {
+                items: 3,
+                nav: false
+            },
+    
+            1468: {
+                items: 5,
+                nav: false
+            }
+        }
+    });
+
+    $('#aside__used').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: false,
+        autoplay : true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        navText: [
+            "<i class='bx bx-first-page'></i>",
+            "<i class='bx bx-last-page' ></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            768: {
+                items: 3,
+                nav: false
+            },
+    
+            1468: {
+                items: 5,
+                nav: false
+            }
+        }
+    });
+});
